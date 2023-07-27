1b810da7694cd3d0b5f89981b8a2169b689a810e
John Ray <johnraychannel@gmail.com>
2023-07-27 16:33:05 +1000

    Complete admin tools for users

--- src/api/dto/previewItem.dto.ts
+++
@@ -1,7 +0,0 @@
-export interface previewItemDto {
-    readonly _id: string
-    readonly image: string
-    readonly name: string
-    readonly description: string
-    readonly favorites: number
-}
